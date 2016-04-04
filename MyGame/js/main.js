ec8f873db6fc36070ae23eb101333c445e0ceda5
Podmaskov <podmaskov@gmail.com>
2016-04-04 09:07:14 +0300

    Game

--- MyGame/js/main.js
+++ MyGame/js/main.js
@@ -1,9 +1,18 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
-
-var guess;
-var hits = 0;
-var guesses = 0;
-
-var isSunk = false;
+// var location1 = 3;
+// var location2 = 4;
+// var location3 = 5;
+//
+// var guess;
+// var hits = 0;
+// var guesses = 0;
+//
+// var isSunk = false;
+function mSec () {
+  var a = 0;
+while (a != 999) {
+  a++
+  console.log (a);
+}
+}
+setTimeout(mSec, 1);
+mSec();
