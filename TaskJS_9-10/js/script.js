2d415a4d307a27b333035aad414845838e365357
Artem Podmaskov <podmaskov@gmail.com>
2016-04-20 23:06:13 +0300

    Task 9-10

--- TaskJS_9-10/js/script.js
+++ TaskJS_9-10/js/script.js
@@ -1,1 +1,13 @@
+$(function(){
+  $('.jcarousel').jcarousel();
+  $('.jcarousel').jcarousel({
+    wrap: 'circular',
+}, 0);
+  $('.jcarousel-prev').jcarouselControl({
+    target: '-=1'
+    });
 
+    $('.jcarousel-next').jcarouselControl({
+        target: '+=1'
+    });
+});
