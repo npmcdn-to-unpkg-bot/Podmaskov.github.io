d1054a106ef5f97c43c78c658ae3da0d9c7d03bd
Artem Podmaskov <podmaskov@gmail.com>
2016-05-15 22:46:46 +0300

    TaskJS_15-16 part1 fix bag

--- TaskJS_15-16/js/script.js
+++ TaskJS_15-16/js/script.js
@@ -1,19 +1,25 @@
 $(function(){
-  var data;
-$('.search').on('click',function(){
-   value = $('.data').val();
+  function callBack () {
+    var value = $('.data').val();
 
-request = $.ajax({
-  url:'https://pixabay.com/api/',
-  data: { key: '2573416-853329f61ff5a3b6beba25569',
-          q: value,
-        },
-  success: function(data){
-          var html = $('#template').html();
-          var content = tmpl(html,data);
-          $('body').append(content);
-        }
+    var request = $.ajax({
+      url:'https://pixabay.com/api/',
+      data: { key: '2573416-853329f61ff5a3b6beba25569',
+            q: value,
+            },
+      success: function(data){
+            var html = $('#template').html();
+            var content = tmpl(html,data);
+            $('body').append(content);
+          }
+  });
+};
+$('.search').on('click',callBack);
+
+$('.data').keypress(function(e){
+  if (e.which === 13 ){
+    callBack();
+  }
 });
 
-  });
     });
