6d81cfd3149b3d07f31a9036e139931ebc2bc405
Artem Podmaskov <podmaskov@gmail.com>
2016-06-20 15:24:03 +0300

    final mobile

--- Final/js/script.js
+++ Final/js/script.js
@@ -1,3 +1,4 @@
+// Carousel
 $(function() {
     $('.jcarousel').jcarousel({
         wrap: 'circular'
@@ -6,4 +7,53 @@ $(function() {
             target: '+=1',
             autostart: true
         });
+});
+// request
+var collectionOfPictures = [];
+var collectionOfTags = [];
+$(function () {
+  var value;
+  $('.nav_button').on('click', function() {
+    $('.grid').remove();
+    value = $('.nav__value').val();
+    images ();
+  });
+
+  $('.nav__value').keypress(function(e){
+    if (e.which === 13 ){
+      $('.grid').remove();
+      value = $('.nav__value').val();
+      images ();
+    }
+      });
+  function images () {
+    $.ajax({
+            url:'https://pixabay.com/api/',
+            data: { key: '2573416-853329f61ff5a3b6beba25569',
+                    q: value
+                  },
+                  success: function (data) {
+                    console.log(data);
+                      for(var i=0; i<7; i++) {
+                      var arr = data.hits;
+                      collectionOfPictures[i] = arr[i].webformatURL;
+                      collectionOfTags[i] = arr[i].tags;
+                    }
+                    var html = $('#template').html();
+                    var content = tmpl(html,collectionOfPictures,collectionOfTags);
+                    $('.wrapForTemplate').append(content);
+                  }
+            });
+          }
+  images();
+});
+console.log(typeof images)
+// masonry
+$(function () {
+  console.log($('.grid'));
+  $('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer'
+  });
 });
